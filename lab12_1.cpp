1091ba2fda5d1c1abcf9c0d9e47b830c8106d9e2
TonPKG <piyaphat_kh@cmu.ac.th>
2021-12-21 14:23:03 +0700

    lab12_1.cpp Completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,48 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+
+int main(){
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    srand(time(0));
+    int x = rand()%9+1;
+    
+    string Grade;
+    if(x == 1){
+        Grade = "A";
+    }
+    else if(x == 2){
+        Grade = "B+";
+    }
+    else if(x == 3){
+        Grade = "B";
+    }
+    else if(x == 4){
+        Grade = "C+";
+    }
+    else if(x == 5){
+        Grade = "C";
+    }
+    else if(x == 6){
+        Grade = "D+";
+    }
+    else if(x == 7){
+        Grade = "D";
+    }
+    else if(x == 8){
+        Grade = "F";
+    }else{
+        Grade = "W";
+    }
+    cout << "You will get "<< Grade <<" in this 261102.";
+    return 0;
+}
+
+
